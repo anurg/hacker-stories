7f0743711b1bb10eb995a804467d20d2144196e6
anurg <anurg@yahoo.com>
2022-12-20 22:13:24 +0530

    commit on 20-12-2022

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ const welcome = {
 function getTitle(title) {
   return title;
 }
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 function App() {
   return (
     // <div className="App">
@@ -32,6 +50,21 @@ function App() {
       </h3>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
+      {/* comment example */}
+      <ul>
+        {list.map(function (item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
