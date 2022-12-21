85d81e7fe0768daad984ecbd92dbf9ab1141677d
anurg <anurg@yahoo.com>
2022-12-21 08:14:07 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -4,52 +4,55 @@ const welcome = {
   greeting: "Hey",
   title: "React 18",
 };
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "jordan Walke",
-    num_comments: 3,
-    poinits: 4,
-    ObjectId: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov,Andrew Clark",
-    num_comments: 6,
-    points: 8,
-    ObjectId: 1,
-  },
-];
+
 function getTitle(title) {
   return title;
 }
 function App() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "jordan Walke",
+      num_comments: 3,
+      poinits: 4,
+      ObjectId: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov,Andrew Clark",
+      num_comments: 6,
+      points: 8,
+      ObjectId: 1,
+    },
+  ];
   return (
     <div>
       <h3>Hello {getTitle("React 16")}!! </h3>
       <h2>
         {welcome.greeting} {welcome.title}
       </h2>
-      <List />
+      <List list={stories} />
       <Search />
-      <List />
     </div>
   );
 }
 function Search() {
+  const handleChange = function (event) {
+    console.log(event.target.value);
+  };
   return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 }
-function List() {
+function List(props) {
   return (
     <ul>
-      {list.map(function (item) {
+      {props.list.map(function (item) {
         return (
           <div>
             <a href={item.url}>{item.title}</a>
