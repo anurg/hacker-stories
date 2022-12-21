b053c3ee077eee832bd6f6869185e2b36051752d
anurg <anurg@yahoo.com>
2022-12-21 08:02:33 +0530

    Practice

--- src/App.js
+++ src/App.js
@@ -1,72 +1,62 @@
-// import logo from "./logo.svg";
 import "./App.css";
-// const title = "React";
+const title = "React";
 const welcome = {
   greeting: "Hey",
-  title: "React",
+  title: "React 18",
 };
-function getTitle(title) {
-  return title;
-}
 const list = [
   {
     title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
+    url: "https://reactjs.org",
+    author: "jordan Walke",
     num_comments: 3,
-    points: 4,
-    objectID: 0,
+    poinits: 4,
+    ObjectId: 0,
   },
   {
     title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
+    url: "https://redux.js.org",
+    author: "Dan Abramov,Andrew Clark",
+    num_comments: 6,
+    points: 8,
+    ObjectId: 1,
   },
 ];
+function getTitle(title) {
+  return title;
+}
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
-      <h3>
-        {welcome.greeting} {welcome.title} !! Hello {getTitle("React!!!!")}
-      </h3>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
-      {/* comment example */}
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <h3>Hello {getTitle("React 16")}!! </h3>
+      <h2>
+        {welcome.greeting} {welcome.title}
+      </h2>
+      <List />
+      <Search />
+      <List />
     </div>
   );
 }
-
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <div>
+            <a href={item.url}>{item.title}</a>
+          </div>
+        );
+      })}
+    </ul>
+  );
+}
 export default App;
