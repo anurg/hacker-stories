8f41f75a800d5988d8c9fda186cc16485ccdac64
Anurag <anurg@yahoo.com>
2025-04-28 23:37:03 +0530

    React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 const App = () => {
   const stories = [
     {
@@ -18,22 +18,25 @@ const App = () => {
       objectID: 1,
     },
   ];
- 
-  const [searchTerm, setSearchTerm] = useState(localStorage.getItem("search")||"React");
- 
+
+  const [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem("search") || "React"
+  );
+
   const handleSearch = (event) => {
-    setSearchTerm(event.target.value)
-    
+    setSearchTerm(event.target.value);
   };
-  useEffect(()=>{
-    localStorage.setItem("search",event.target.value)
-  },[searchTerm]);
-  
-  const filteredStories = stories.filter(story=>story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+  useEffect(() => {
+    localStorage.setItem("search", searchTerm);
+  }, [searchTerm]);
+
+  const filteredStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <div>
       <h1>My Hacker Stories - {searchTerm} </h1>
-      
+
       <Search onSearch={handleSearch} searchTerm={searchTerm} />
       <hr />
       <List list={filteredStories} />
@@ -41,29 +44,29 @@ const App = () => {
   );
 };
 
-const Search = ({onSearch,searchTerm}) => 
-   (
-    <div>
-      <label htmlFor="search">Search:</label>
-      <input type="text" id="search" onChange={onSearch} value={searchTerm} />
-      <p>
-        Seraching for <strong>{searchTerm}</strong>
-      </p>
-    </div>
-  );
+const Search = ({ onSearch, searchTerm }) => (
+  <>
+    <label htmlFor="search">Search:</label>
+    <input type="text" id="search" onChange={onSearch} value={searchTerm} />
+    <p>
+      Seraching for <strong>{searchTerm}</strong>
+    </p>
+  </>
+);
 
-
-const List = ({list}) => {
+const List = ({ list }) => {
   return (
     <>
       <ul>
-        {list.map((item) => <Item key={item.objectID} item={item}/>)}
+        {list.map((item) => (
+          <Item key={item.objectID} item={item} />
+        ))}
       </ul>
     </>
   );
 };
 
-const Item = ({item}) => {
+const Item = ({ item }) => {
   return (
     <>
       <li>
@@ -79,5 +82,3 @@ const Item = ({item}) => {
 };
 
 export default App;
-
-
